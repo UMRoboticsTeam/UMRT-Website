be974e49e8ccacca68b5b42f608ac54d6fe816c8
Backback12 <cepagtakhan@gmail.com>
2025-02-19 23:52:15 -0600

    Updated Team Photos & People
    Added balcony photos for people who had them.
    
    Goodbye Abraham as Embedded Lead 💯 Welcome Niko and Ernest as Embedded Co-Leads! 💯

--- .output/public/_nuxt/D9bYTIF0.js
+++
@@ -1,1 +0,0 @@
-import"./B9aKYzkL.js";const e=""+new URL("TeamPhoto-2.CQdfRxoK.jpg",import.meta.url).href;export{e as H};
---
+++ .output/public/_nuxt/DKfBN_Ec.js
@@ -0,0 +1,1 @@
+import"./C-2Qcb9U.js";const e=""+new URL("TeamPhoto-2.CQdfRxoK.jpg",import.meta.url).href;export{e as H};
